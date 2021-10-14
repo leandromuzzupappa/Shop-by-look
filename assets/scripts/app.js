a0e987f110c642704f97cac88ec1d3711cfcd92c
Leandro Muzzupappa <lmuzzupappa@glamit.com.ar>
2021-10-14 17:20:19 -0300

    First comit

---
+++ assets/scripts/app.js
@@ -0,0 +1,224 @@
+const screenRes = {
+    isMobile: window.matchMedia("screen and (max-width: 700px)").matches,
+    isTablet: window.matchMedia("screen and (max-width: 1000px)").matches,
+    isDesktop: window.matchMedia("screen and (min-width: 1001px)").matches,
+};
+
+const shopByLookCustomizer = document.querySelector(".shop_by_look_customizer");
+if (shopByLookCustomizer) {
+    console.log("init");
+
+    const sblContentMain = document.querySelector(".sbl__content--main");
+
+    const products = getProducts();
+
+    if (!products) {
+        const productList = sblContentMain.querySelector(".sbl__products");
+        productList.innerHTML =
+            "<p class='sbl__products--empty'>No hay productos seteados</p>";
+    }
+}
+
+function getProducts() {
+    // Get all products from localStorage - shopByLookCustomizer -
+    // if exists returns an array
+    // else returns false
+    const localStorageKey = "shopByLookCustomizer";
+    const productsInLocalStorage = localStorage.getItem(localStorageKey);
+
+    if (
+        productsInLocalStorage === null ||
+        productsInLocalStorage.length === 0
+    ) {
+        return false;
+    }
+
+    return JSON.parse(productsInLocalStorage);
+}
+
+function saveProduct(event) {
+    event.preventDefault();
+
+    const localStorageKey = "shopByLookCustomizer";
+
+    // Get the custom data
+    const productCreatePanel = document.querySelector(".sbl__content--create");
+
+    const productData = {};
+    const productInputs = productCreatePanel.querySelectorAll("input");
+
+    productInputs.forEach((input) => {
+        switch (input.id) {
+            case "sbl__product--id":
+                productData.id = input.value;
+                break;
+
+            case "sbl__product--name":
+                productData.name = input.value;
+                break;
+
+            case "sbl__product--price":
+                productData.price = input.value;
+                break;
+
+            case "sbl__product--link":
+                productData.link = input.value;
+                break;
+
+            case "sbl__product--top":
+                productData.top = input.value;
+                break;
+
+            case "sbl__product--left":
+                productData.left = input.value;
+                break;
+
+            case "sbl__product--background":
+                productData.background = input.value;
+                break;
+
+            case "sbl__product--textcolor":
+                productData.color = input.value;
+                break;
+        }
+    });
+
+    // Store data in localStorage
+    const productsStored = getProducts();
+
+    if (!productsStored) {
+        localStorage.setItem(localStorageKey, JSON.stringify([productData]));
+        closeCreateProduct();
+        return;
+    }
+
+    // Update one
+    const hasProductUpdate = productsStored.find(
+        (product) => product.id === productData.id
+    );
+
+    if (hasProductUpdate) {
+        const productsUpdated = productsStored.map((product) => {
+            if (product.id === productData.id) {
+                product = productData;
+            }
+
+            return product;
+        });
+
+        localStorage.setItem(localStorageKey, JSON.stringify(productsUpdated));
+        closeCreateProduct();
+        return;
+    }
+
+    productsStored.push(productData);
+    localStorage.setItem(localStorageKey, JSON.stringify(productsStored));
+    closeCreateProduct();
+}
+
+function createProduct() {
+    console.log("create");
+
+    const productID = Date.now();
+    createDot(productID);
+
+    const productCreatePanel = document.querySelector(".sbl__content--create");
+    productCreatePanel.classList.add("sbl__content--create--open");
+
+    const productDot = document.querySelector(`#product--${productID}`);
+
+    if (!screenRes.isDesktop) {
+    } else {
+        productDot.addEventListener("mouseenter", () =>
+            handleButtonOpen(productDot)
+        );
+        productDot.addEventListener("mouseleave", () =>
+            handleButtonOpen(productDot)
+        );
+    }
+
+    const productDataUnformated = productCreatePanel.querySelectorAll("input");
+    productDataUnformated.forEach((data) => {
+        data.id === "sbl__product--id" && (data.value = productID);
+
+        data.addEventListener("keyup", () => updateDot(data, productDot));
+    });
+}
+
+function deleteProduct(el) {
+    console.log("delete");
+    console.log(el);
+}
+
+function closeCreateProduct() {
+    const productCreatePanel = document.querySelector(".sbl__content--create");
+    productCreatePanel.classList.remove("sbl__content--create--open");
+
+    const form = document.querySelector("#sbl__content--form");
+    form.reset();
+}
+
+function createDot(dotID) {
+    const preview = document.querySelector(".sbl__preview");
+    const previewArea = preview.querySelector(".sbl__preview--area");
+
+    previewArea.innerHTML += `
+        <button id="product--${dotID}" class="product__btn">
+            <p>
+                <span>Colaless</span>
+                <strong> $2.500 </strong>
+            </p>
+        </button>
+    `;
+}
+
+function updateDot(data, target) {
+    const dataTarget = data.id;
+
+    const productName = target.querySelector("span");
+    const productPrice = target.querySelector("strong");
+
+    switch (dataTarget) {
+        case "sbl__product--name":
+            productName.innerHTML = data.value;
+            break;
+
+        case "sbl__product--price":
+            productPrice.innerHTML = data.value;
+            break;
+
+        case "sbl__product--link":
+            target.dataset.link = data.value;
+            break;
+
+        case "sbl__product--top":
+            target.style.top = data.value;
+            break;
+
+        case "sbl__product--left":
+            target.style.left = data.value;
+            break;
+
+        case "sbl__product--background":
+            target.style.background = data.value;
+            break;
+
+        case "sbl__product--textcolor":
+            target.querySelector("p").style.color = data.value;
+            break;
+    }
+}
+
+function handleButtonOpen(button) {
+    button.classList.toggle("open");
+
+    const isButtonOpen = button.classList.contains("open");
+
+    if (isButtonOpen) {
+        button.addEventListener(
+            "click",
+            () => (window.location.href = button.dataset.link)
+        );
+        return;
+    }
+}
